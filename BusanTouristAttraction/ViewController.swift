4a1b6f355e4bcbd8e125a42baefb8ac7b6796e24
정순우 <soonwoo2714@gmail.com>
2018-11-29 13:05:36 +0900

    Initial Commit

---
+++ BusanTouristAttraction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BusanTouristAttraction
+//
+//  Created by 정순우 on 29/11/2018.
+//  Copyright © 2018 정순우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
